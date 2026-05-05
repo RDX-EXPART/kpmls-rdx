b8388fce8716525be5ff2b5de769d07cc6854ccf
GK-BOTZ <gauravermagk@gmail.com>
2026-05-06 01:40:38 +0530

    update __init__.py

--- bot/helper/utils/__init__.py
+++ bot/helper/utils/__init__.py
@@ -232,7 +232,6 @@ def _pick_best_link(links: dict):
 
     for p in priority:
         if p in lowered:
-            logger.info(f'Link: {lowered[p]}')
             return lowered[p]
 
     return next(iter(links.values()))
